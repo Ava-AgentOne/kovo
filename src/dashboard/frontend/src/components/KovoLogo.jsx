227913583d3d24c8293e1017d12ca0b7ea8fae71
Ava-AgentOne <ava.esamagent@gmail.com>
2026-03-26 09:52:57 +0000

    fix: KovoLogo viewBox clipping on antenna circles

--- src/dashboard/frontend/src/components/KovoLogo.jsx
+++ src/dashboard/frontend/src/components/KovoLogo.jsx
@@ -3,7 +3,7 @@ export default function KovoLogo({ size = 36, animate = true }) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 120 130"
+      viewBox="-3 -6 126 140"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={animate ? 'kovo-logo' : ''}
